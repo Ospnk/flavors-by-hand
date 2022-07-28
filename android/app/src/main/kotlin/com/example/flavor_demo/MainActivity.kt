2b5095a5f00b9ad7b9d5e74c2ce0ec5280cf7c0e
Ospnk <86660063+Ospnk@users.noreply.github.com>
2022-07-28 19:18:15 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/flavor_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flavour.flavordemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_stage.dart
@@ -0,0 +1,6 @@
+import 'package:flavor_demo/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp(flavor: "Stage"));
+}
